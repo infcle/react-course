a54de1ac98a489b0873b5f3c1813f99a7b7b8dbb
Elmer <infcle@gmail.com>
2025-05-28 21:53:52 -0400

    feacture: Creacion de nuevo componente

--- src/index.js
+++ src/index.js
@@ -4,4 +4,26 @@ import ReactDOM from 'react-dom/client'
 // Para poder crear elemkentos de React en el DOM
 // Importante: ReactDOM.createRoot es la nueva forma de renderizar en React 18
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<h1>Hello, World!</h1>)
+// root.render(<h1>Hello, World!</h1>)
+
+
+function Greting() {
+    return <div>
+        <h1>Hello, World!</h1>
+        <p>Este es un componente de React</p>
+    </div>
+
+
+}
+
+root.render(Greting())
+// root.render(<Greting />)
+
+// otra manera de llamar a un componente
+root.render(
+    <div>
+        {Greting()}
+        <Greting />
+        <Greting></Greting>
+    </div>
+)
