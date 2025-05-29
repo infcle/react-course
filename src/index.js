d821122579e19bbd2204095034676af97fc6d550
Elmer <infcle@gmail.com>
2025-05-28 21:39:10 -0400

    feacture: hola mundo react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Para poder crear elemkentos de React en el DOM
+// Importante: ReactDOM.createRoot es la nueva forma de renderizar en React 18
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<h1>Hello, World!</h1>)
